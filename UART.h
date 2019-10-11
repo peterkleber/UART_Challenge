b8a8217a656b2994502c9e8c5690b30528d7af66
peterkleber <38935088+peterkleber@users.noreply.github.com>
2019-10-11 19:49:58 +0200

    UART Driver

---
+++ UART.h
@@ -0,0 +1,24 @@
+/*
+ * UART.h
+ *
+ *  Created on: Oct 11, 2019
+ *      Author: PeterKleber
+ */
+
+#ifndef UART_H_
+#define UART_H_
+
+#include <avr/io.h>
+#include "UART_cnfg.h"
+
+typedef enum {
+	NOK, OK
+} Std_Func_t;
+
+Std_Func_t UART_init();
+
+Std_Func_t UART_send(const uint8 Data_to_sent);
+
+Std_Func_t UART_recieve(uint8 *Data_recieved);
+
+#endif /* UART_H_ */
