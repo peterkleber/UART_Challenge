480d9b4b9a75c19fff6bb45c1498704fe3714818
Ahmed__Ehab <ahmedehab11@gmail.com>
2019-10-14 18:17:01 +0200

    LCD has been updated to another Version

--- LCD.c
+++ LCD.c
@@ -1,287 +1,185 @@
-/*******************************************************************************
- *                      	Included Libraries                                 *
- *******************************************************************************/
 #include "LCD.h"
-/*******************************************************************************
- *                      Functions Prototypes                                   *
- *******************************************************************************/
 
-/*******************************************************************************
- * Function Name:	LCD_init
- *
- * Description: 	
- * 			
- * Inputs:			
- *
- * Outputs:			
- *
- * Return:			
- *******************************************************************************/
-void LCD_init(void) {
-
-	/* Data Direction Configuration */
-
-	DDRA = 0xFF;
-
-	_delay_us(50);
-
-	/* Initialize LCD to work in 4_bit mode */
-	LCD_sendCommand(Init_4bit_1);
-	_delay_us(50);
-
-	LCD_sendCommand(Init_4bit_2);
-	_delay_us(50);
-
-	LCD_sendCommand(FunctionSet4bit);
-	_delay_us(50);
-
-	/* LCD Initialization */
-	LCD_sendCommand(Return_home); /* Start from home Position */
-	_delay_us(50);
-
-	LCD_sendCommand(Displayon_Cursoroff); /* LCD display On */
-	_delay_us(50);
-
-	LCD_sendCommand(Display_noshift); /*  mode set */
-	_delay_us(50);
-
-	LCD_sendCommand(Clear_display); /* Clear LCD display */
-	_delay_us(2000);
-
+void Enable()
+{
+	CMD_PORT|=(1<<EN);
+	_delay_us(700);
+	CMD_PORT&=~(1<<EN);
+	_delay_us(700);
 }
 
-/*******************************************************************************
- * Function Name:	LCD_sendCommand
- *
- * Description: 	
- * 			
- * Inputs:			
- *
- * Outputs:			
- *
- * Return:			
- *******************************************************************************/
-void LCD_sendCommand(uint8_t command) {
-	/* Control Signals Configuration */
-
-	PORTA &= (~(1 << LCD_RS));
-	PORTA &= (~(1 << LCD_RW));
-	PORTA |= (1 << LCD_E);
-
-	_delay_us(50);
-
-	/* Sending High nibble */
-	PORTA = (PORTA & 0x0F) | (command & 0xF0);
-	_delay_us(50);
-
-	PORTA &= (~(1 << LCD_E));/* Disable Signal */
-	_delay_us(50);
-
-	PORTA |= (1 << LCD_E); /* Enable Signal */
-	_delay_us(50);
-
-	/* Sending Low nibble */
-	PORTA = (PORTA & 0x0F) | ((command & 0x0F) << 4);
-	_delay_us(50);
-
-	PORTA &= (~(1 << LCD_E));/* Disable Signal */
-	_delay_us(50);
-
-	PORTA |= (1 << LCD_E); /* Enable Signal */
-	_delay_us(50);
+void DATA_WRT_4bit(uint8_t data)
+{
+	CMD_PORT|=(1<<RS);
+	DATA_PORT&=(0x0f);
+	DATA_PORT|=(data & 0xf0);
+	Enable();
+	DATA_PORT&=(0x0f);
+	DATA_PORT|=((data & 0x0f)<<4);
+	Enable();
 }
 
-/*******************************************************************************
- * Function Name:	LCD_displayChar
- *
- * Description: 	
- * 			
- * Inputs:			
- *
- * Outputs:			
- *
- * Return:			
- *******************************************************************************/
-void LCD_displayChar(uint8_t character) {
-	/* Control Signals Configuration */
-
-	PORTA |= (1 << LCD_RS); /* Select data register */
-	PORTA &= (~(1 << LCD_RW)); /*disable Write operation */
-	PORTA |= (1 << LCD_E);/* Enable Signal */
-	_delay_us(50);
-
-	/* Sending High nibble */
-	PORTA = (PORTA & 0x0F) | (character & 0xF0);
-	_delay_us(50);
-
-	PORTA &= (~(1 << LCD_E));/* Disable Signal */
-	_delay_us(50);
-
-	PORTA |= (1 << LCD_E);/* Enable Signal */
-	_delay_us(50);
-
-	/* Sending low nibble */
-	PORTA = (PORTA & 0x0F) | ((character & 0x0F) << 4);
-	_delay_us(50);
-
-	PORTA &= (~(1 << LCD_E));/* Disable Signal */
-	_delay_us(50);
-
-	PORTA |= (1 << LCD_E);/* Enable Signal */
-	_delay_us(50);
-
+void DATA_WRT_8bit(uint8_t data)
+{
+	CMD_PORT|=(1<<RS);
+	DATA_PORT&=~(0xff);
+	DATA_PORT|=data;
+	Enable();
 }
 
-/*******************************************************************************
- * Function Name:	LCD_gotoRowColumn
- *
- * Description: 	
- * 			
- * Inputs:			
- *
- * Outputs:			
- *
- * Return:			
- *******************************************************************************/
-void LCD_gotoRowColumn(uint8_t LINE, uint8_t COLUMN) {
-	if ((COLUMN > COLUMN_MIN) && (COLUMN <= COLUMN_MAX)) {
-		switch (LINE) {
-		case LINE_1:
-			LCD_sendCommand(( lcd_line_1_address) + (COLUMN));
-			break;
+void CMD_WRT_4bit(uint8_t cmd)
+{
+	CMD_PORT &= ~(1<<RS);
+	DATA_PORT &= ~(0xf0);
+	DATA_PORT|=(cmd & 0xf0);
+	Enable();
+	DATA_PORT&=~(0xf0);
+	DATA_PORT|=((cmd & 0x0f)<<4);
+	Enable();
+}
 
-		case LINE_2:
-			LCD_sendCommand(( lcd_line_2_address) + (COLUMN));
-			break;
+void CMD_WRT_8bit(uint8_t cmd)
+{
+	CMD_PORT&=~(1<<RS);
+	DATA_PORT&=~(0xff);
+	DATA_PORT|=cmd;
+	Enable();
+}
 
-		default:
-			break;
+void LCD_INIT_4bit()
+{
+	CMD_DDR |= (1<<RS)|(1<<EN)|(1<<RW);
+	DATA_DDR |=(0xf0);
+	DATA_PORT &= ~(0xf0);
+	CMD_PORT &= ~(1<<RS);	// RS <= 0 to send commands
+	CMD_PORT &= ~(1<<EN);
+	CMD_PORT &= (~1<<RW);
+	_delay_ms(20);
+	
+	DATA_PORT &= ~(0xf0);
+	DATA_PORT |= (0x30);
+	Enable();
+	_delay_us(150);
+	DATA_PORT &= ~(0xf0);
+	DATA_PORT |= (0x30);
+	Enable();
+	_delay_us(150);
+	DATA_PORT &= ~(0xf0);
+	DATA_PORT |= (0x30);
+	Enable();
+	_delay_us(150);
+	
+	DATA_PORT &= ~(0xf0);
+	DATA_PORT |= (0x20);
+	Enable();
+	_delay_ms(2);
+	
+	CMD_WRT_4bit(0x2C);
+	_delay_us(45);
+	
+	CMD_WRT_4bit(0x0E);  // Display --> oN , Cursor --> Off , Cursor blinking --> off
+	_delay_us(45);
+	
+	CMD_WRT_4bit(0x06);  // Entry set mode : Display shift --> Off , Increment by 1
+	_delay_us(45);
+	
+	CMD_WRT_4bit(0x01);  // Clear LCD
+	_delay_us(45);
+	
+	CMD_WRT_4bit(0x1C);  // Cursor Display shift : shift --> Cursor ,  Shift --> Right
+	_delay_us(120);
+	
+	
+	
+}
+
+void LCD_CLR()
+{
+	CMD_WRT_4bit(0x01);
+	_delay_us(45);
+}
+
+void LCD_Cursor_Home()
+{
+	CMD_WRT_4bit(0x02);
+	_delay_ms(2);
+}
+
+void LCD_CUSRSOR_POS(uint8_t row , uint8_t col)
+{
+	CMD_WRT_4bit((0x80)|(col+(0x40*row)));
+}
+
+void LCD_CHAR_DISP(uint8_t row , uint8_t col, uint8_t data)
+{
+	LCD_CUSRSOR_POS(row,col);
+	DATA_WRT_4bit(data);
+}
+
+
+void LCD_String_DISP(uint8_t row , uint8_t col ,char *s)
+{
+	LCD_CUSRSOR_POS(row ,col);
+	while(*s)
+	{
+		DATA_WRT_4bit(*s++);
+	}
+	
+}
+
+void LCD_NUM_DISP(uint8_t row , uint8_t col, uint32 num)
+{
+	LCD_CUSRSOR_POS(row ,col);
+	if (0<=num)
+	{
+		
+		if (num<=9) DATA_WRT_4bit(num+0x30);
+		else if(num<=99)
+		{
+			DATA_WRT_4bit((num/10)+0x30);
+			DATA_WRT_4bit((num%10)+0x30);
 		}
-	} else {
-		/* Do Nothing */
-	}
-
-}
-
-/*******************************************************************************
- * Function Name:	LCD_displayString
- *
- * Description: 	
- * 			
- * Inputs:			
- *
- * Outputs:			
- *
- * Return:			
- *******************************************************************************/
-void LCD_displayString(uint8_t string[]) {
-	uint8_t iterator = 0;
-	while (string[iterator] != '\0') {
-		LCD_displayChar(string[iterator]);
-		iterator++;
-	}
-}
-
-/*******************************************************************************
- * Function Name:	LCD_displayStringRowColumn
- *
- * Description: 	
- * 			
- * Inputs:			
- *
- * Outputs:			
- *
- * Return:			
- *******************************************************************************/
-void LCD_displayStringRowColumn(uint8_t string[], uint8_t row, uint8_t column) {
-	LCD_gotoRowColumn(row, column);
-	LCD_displayString(string);
-}
-
-/*******************************************************************************
- * Function Name:	LCD_clear
- *
- * Description: 	
- * 			
- * Inputs:			
- *
- * Outputs:			
- *
- * Return:			
- *******************************************************************************/
-void LCD_clear(void) {
-	LCD_sendCommand(Clear_display);
-	//_delay_us(2000);
-	//Timer_enmDelayBlocking(TIMER_GENERAL_DELAY, TIMER_PERIOD_10_MICRO_SECOND, 200);
-	//LCD_sendCommand(Return_home);
-}
-
-// a function to reverse a string
-void reverse(uint8_t *str, uint8_t length) {
-	uint8_t start = 0;
-	uint8_t end = length - 1;
-	while (start < end) {
-		uint8_t temp = str[start];
-
-		str[start] = str[end];
-
-		str[end] = temp;
-
-		start++;
-		end--;
-	}
-
-}
-
-uint8_t* integer_to_string(sint32 number, uint8_t *str, uint8_t base) {
-	// index for length
-
-	uint8_t i = 0;
-	uint8_t isNegative = 0;
-
-	// Handle 0 explicitely
-
-	if (number == 0) {
-		str[i++] = '0';
-		str[i] = '\0';  // the end of the string
-		return str;
-	}
-
-	// negative numbers are handled only with
-	// base 10. Otherwise numbers are considered unsigned
-
-	if (number < 0 && base == 10) {
-		isNegative = 1;
-		number = -number; // to get the absolute value of the number
-	}
-
-	while (number != 0) {
-
-		uint8_t remainder = number % base;
-		if (remainder > 9) {
-			str[i++] = (remainder - 10) + 'A';  // for hexa-decimal only
-
-		} else {
-			str[i++] = remainder + '0';
+		else if(num<=999)
+		{
+			DATA_WRT_4bit((num/100)+0x30);
+			DATA_WRT_4bit(((num/10)%10)+0x30);
+			DATA_WRT_4bit((num%10)+0x30);
 		}
-		number = number / base;
+		
+		else if(num<=99999)
+		{
+			DATA_WRT_4bit( ( (num/10000) ) +0x30);
+			DATA_WRT_4bit( ( (num/1000) %10 )+0x30);
+			DATA_WRT_4bit( ( (num/100) %10 ) +0x30);
+			DATA_WRT_4bit( ( (num/10) %10 )+0x30);
+			DATA_WRT_4bit((num%10)+0x30);
+		}
 	}
-
-	if (isNegative) {
-		str[i++] = '-';
+	
+	if (0>num)
+	{
+		num = -1*num;
+		
+		if (num<=9)
+		{
+			DATA_WRT_4bit(0x2d);
+			DATA_WRT_4bit(num+0x30);
+		}
+		else if(num<=99)
+		{
+			DATA_WRT_4bit(0x2d);
+			DATA_WRT_4bit((num/10)+0x30);
+			DATA_WRT_4bit((num%10)+0x30);
+		}
+		else if(num<=999)
+		{
+			DATA_WRT_4bit(0x2d);
+			DATA_WRT_4bit((num/100)+0x30);
+			DATA_WRT_4bit(((num/10)%10)+0x30);
+			DATA_WRT_4bit((num%10)+0x30);
+		}
+		
 	}
-
-	str[i] = '\0';  // the end of the string
-
-	reverse(str, i); // where i  index for the length of the string
-
-	return str;
-}
-
-void LCD_Write_Integer_Number(sint32 number) {
-	uint8_t str[16];
-	integer_to_string(number, str, 10);
-
-	LCD_displayString((uint8_t*) str);
-
+	
+	_delay_ms(500);
 }
--- LCD.h
+++ LCD.h
@@ -1,58 +1,36 @@
+
 #ifndef LCD_H_
 #define LCD_H_
 
-/*******************************************************************************
- *                      	Included Libraries                                 *
- *******************************************************************************/
+#include "std_types.h"
 #include <avr/io.h>
 #include <util/delay.h>
-#include "std_types.h"
-/*******************************************************************************
- *                      Preprocessor Macros                                    *
- *******************************************************************************/
 
-#define Clear_display 0x01
-#define Return_home 0x02
-#define Display_noshift 0x06
-#define Display_shift 0x07
-#define Display_off 0x08
-#define Displayon_Cursoroff	0x0C
-#define Display_on 0x0E
-#define Init_4bit_1 0x33
-#define Init_4bit_2 0x32
-#define FunctionSet4bit 0x28
-#define FunctionSet8bit 0x38
-#define SET_CGRAM_addr 0x40
-#define SET_DDRAM_addr 0x80
 
-#define Register_Select 1
-#define Read_Write      2
-#define Enable_LCD      3
 
-#define LCD_RS    1
-#define LCD_RW    2
-#define LCD_E     3
 
-#define lcd_line_1_address  0x7F       
-#define lcd_line_2_address  0xBF       
+#define DATA_PORT PORTA
+#define CMD_PORT PORTA
 
-#define LINE_1      1
-#define LINE_2      2
+#define DATA_DDR DDRA
+#define CMD_DDR  DDRA
+#define RS 1
+#define RW 2
+#define EN 3
 
-#define COLUMN_MIN   0
-#define COLUMN_MAX   16
 
-/*******************************************************************************
- *                      Functions Prototypes                                   *
- *******************************************************************************/
+void Enable();
+void CMD_WRT_8bit(uint8_t cmd);
+void CMD_WRT_4bit(uint8_t cmd);
+void DATA_WRT_8bit(uint8_t data);
+void DATA_WRT_4bit(uint8_t data);
+void LCD_INIT_4bit();
+void LCD_CLR();
+void LCD_Cursor_Home();
+void LCD_CUSRSOR_POS(uint8_t row , uint8_t col);
+void LCD_CHAR_DISP(uint8_t row , uint8_t col , uint8_t data);
+void LCD_String_DISP(uint8_t row , uint8_t col ,char *s);
+void LCD_NUM_DISP(uint8_t row , uint8_t col ,uint32 num);
 
-void LCD_init(void);
-void LCD_sendCommand(uint8 command);
-void LCD_displayChar(uint8 character);
-void LCD_displayString(uint8 string[]);
-void LCD_displayStringRowColumn(uint8 string[], uint8 row, uint8 column);
-void LCD_gotoRowColumn(uint8 LINE, uint8 COLUMN);
-void LCD_clear(void);
-void LCD_Write_Integer_Number(sint32 number);
 
-#endif /* LCD_H_ */
+#endif
