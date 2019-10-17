5ef76908b9709f6560248237097bd6ed76e11e5a
Ahmed__Ehab <ahmedehab11@gmail.com>
2019-10-17 21:26:36 +0200

    updating

---
+++ DIO_cnfg.c
@@ -0,0 +1,23 @@
+/*
+ * DIO_cnfg.c
+ *
+ *  Created on: Sep 22, 2019
+ *      Author: HOBA
+ */
+
+
+#include "DIO_cnfg.h"
+
+
+
+DIO_cnfg_t DIO_cnfg_arr[NUM_OF_PINS] =
+{
+	{
+		
+	}
+	/*
+		{PORT_D,PIN4,INPUT,NA,PULLUP,INITIALIZED},
+		{PORT_D,PIN2,INPUT,NA,PULLUP,INITIALIZED}
+	*/
+};
+
--- main.c
+++ main.c
@@ -26,19 +26,19 @@
 
 #define SPEED_TAG	 1
 #define DISTANCE_TAG 2
+#define TIME_TAG	 3
 //----------------- Section of Public Global Variables -------------------------//
-//static uint16 Counter_Sec=0;
+static uint16 Counter_Sec=0;
 //static uint16 Speed = 0;
 static uint16 Current_Distance =0;
 static uint16 Total_Distance = 0;
 static uint16 Previous_Speed=0;
-static uint16 Current_Speed=0;
+static float Current_Speed=0;
 
 //----------------- Section of  Global Flags -------------------------//
 static uint8 Display_Flag =0;
 static uint8 Switch_flag = SYSTEM_OFF;
 static uint8 Busy_Flag = 0;
-//static uint8 Speed_Change_Flag = 0;
 
 //-------------------  Section of Public_Functions APIs Prototype-------------------------------//
 void Second_Count (void);
@@ -53,31 +53,49 @@ void UART_Recieve_Data (void);
 //-------------------  Section of Public_Functions APIs Body Implementation -------------------------------//
 void Second_Count (void)
 {	
+	/*
 	if(Busy_Flag == 0)
-	{
+	{	
 		Busy_Flag = 1;
-		
-		
-		
+			
+		if(Switch_flag == RUNNING)
+		{	
+			Display_Flag = 1;
+			Counter_Sec++;
+			
+			if(Current_Speed != Previous_Speed)
+			{
+				Counter_Sec = 1  ;
+			}
+			
+			Previous_Speed = Current_Speed;
+			Current_Distance = Current_Speed*Counter_Sec;	
+			Total_Distance = Total_Distance + Current_Distance;
+		}
+	
 		Busy_Flag = 0;		
 	}
+	*/
 }
+
+
+
+
 
 void Switch_ON (void)
 {	
 	if(Busy_Flag == 0)
 	{	
-		Busy_Flag = 1;				
-		Switch_flag = SWITCH_ON;
-		Busy_Flag = 0;	
-		LCD_4Bits_Clear();
+	   Busy_Flag = 1;				
+	   Switch_flag = SWITCH_ON;
+	   LCD_4Bits_Clear();
+	
+	   UART_send(READY);			 // Triggering the UART Transmit
+	  // Time_Delay(TIMER1,1,sec);
+		Total_Distance = 0;
+	   Switch_flag = RUNNING ; 
+	   Busy_Flag = 0;	
 	}
-	
-	UART_send(READY);
-				 
-	Switch_flag = RUNNING ;
-	
-	//UART_Transmit_Data ();	 // Triggering the UART Transmit
 }
 
 void Switch_OFF (void)
@@ -87,8 +105,8 @@ void Switch_OFF (void)
 		Busy_Flag = 1;
 		Switch_flag = SWITCH_OFF;
 		Display_Flag = 1;
-		
 		LCD_4Bits_Clear();
+		
 		Busy_Flag = 0;
 	}		
 }
@@ -100,14 +118,9 @@ void Display (void)
 		if(Switch_flag == RUNNING)
 		{	
 			
-			if(Previous_Speed != Current_Speed) 
-			{
-				LCD_4Bits_Clear();
-			}
-			
 			LCD_4Bits_Print_String(1,1,"Distance=");	// Total letters in Word Distance are 9 letters
-			LCD_4Bits_Print_Number(1,10,Current_Distance); 
-			Previous_Speed = Current_Speed;
+			//LCD_4Bits_Print_Number(1,10,Current_Distance); 
+			LCD_4Bits_Print_Number(1,10,Total_Distance);  
 		}
 		
 		else if(Switch_flag == SWITCH_OFF)
@@ -125,7 +138,6 @@ void UART_Recieve_Data (void)
 {
 	if(Busy_Flag == 0)
 	{
-	
 		Busy_Flag = 1;
 		uint8 Data_Recieved = 0;
 		static uint8 Status_Recieved_TAG = 55;
@@ -137,53 +149,24 @@ void UART_Recieve_Data (void)
 			{
 				Status_Recieved_TAG = SPEED_TAG;
 			}
-			
-			else if (Data_Recieved == 'D')
-			{
-				Status_Recieved_TAG = DISTANCE_TAG;
-			}
+
 			else	//Here when the Data_Recieved is a actual Data
 			{
 				if(Status_Recieved_TAG == SPEED_TAG )
 				{
 					Current_Speed= Data_Recieved;
+					Display_Flag = 1;
+					LCD_4Bits_Print_Number(2,10,Current_Speed);
 				}
-				
-				else if (Status_Recieved_TAG == DISTANCE_TAG)
-				{
-					Current_Distance = Data_Recieved;
-				}
-				
 			}
 			
-			Total_Distance = Total_Distance + Current_Distance;
+			Total_Distance += (Current_Speed/2);
 
-			Display_Flag = 1;
 		}
 		
 		Busy_Flag=0;
 	}
 }
-
-/*
-void UART_Transmit_Data (void)
-{
-	if(Busy_Flag == 0)
-	{
-		PORTD ^= (1<<PD4);	//Test Pin
-		Busy_Flag = 1;
-		
-		if(Switch_flag == SWITCH_ON)
-		{
-			 UART_send(READY);
-		 
-			 Switch_flag = RUNNING ;
-		}
-		
-		Busy_Flag = 0;
-	}
-}
-*/
 
 
 //--------------------------------------------------------------------------------------------------------------------------//
@@ -201,14 +184,14 @@ int main()
 		
 	Timer1_Set_Callback ( Second_Count );
 	
-	INTP1_vidSetCallBack(Switch_ON);
-	INTP2_vidSetCallBack(Switch_OFF);
+	INTP1_vidSetCallBack(Switch_OFF);
+	INTP2_vidSetCallBack(Switch_ON);
 	
 	UART_Set_Callback_RX(UART_Recieve_Data);
-	//UART_Set_Callback_TX(UART_Transmit_Data);
-	
+
 	LCD_4Bits_Print_String(2,2,"SYSTEM OFF");	//idle State of the System 
 	
+
 	while(1)
 	{
 		 Display () ;
